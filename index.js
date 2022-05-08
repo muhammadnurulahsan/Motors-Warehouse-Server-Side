8011fec8b9dce563c5abcc787206ceb1414f41e0
muhammadnurulahsan <mdnurulahsanbdbd@gmail.com>
2022-05-08 09:22:05 +0600

    JWT FUnction added

--- index.js
+++ index.js
@@ -18,21 +18,7 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-// JWT Verify Function
-function verifyJWT(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({ message: "unauthorized access" });
-  }
-  const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).send({ message: "Forbidden Access" });
-    }
-    req.decoded = decoded;
-    next();
-  });
-}
+
 
 //Main Function
 async function run() {
@@ -41,7 +27,13 @@ async function run() {
     const itemsCollection = client.db("motorsWarehouse").collection("items");
     console.log("Connected to MongoDB");
 
-  
+    app.post("/login", (req, res) => {
+      const email = req.body;
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "15d",
+      });
+      res.send({ token });
+    });
 
     //getting all products
     app.get("/items", async (req, res) => {
