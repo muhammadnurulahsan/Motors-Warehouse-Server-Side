b78cca95ff119baa1a67febc834a8e3de1d13265
muhammadnurulahsan <mdnurulahsanbdbd@gmail.com>
2022-05-08 15:22:53 +0600

    Fainal

--- index.js
+++ index.js
@@ -18,7 +18,21 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-
+// JWT Verify Function
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 //Main Function
 async function run() {
