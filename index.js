1e19485b4d1fc25b61bb30381b3e5624f53b70a9
muhammadnurulahsan <mdnurulahsanbdbd@gmail.com>
2022-06-04 17:50:55 +0600

    cors origin added

--- index.js
+++ index.js
@@ -7,8 +7,25 @@ const { MongoClient, ObjectId, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 //Middleware
-app.use(cors({ origin: "https://motors-warehouse.web.app" }));
+//middleware
 app.use(express.json());
+app.use(cors());
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.use(cors(corsConfig));
+app.options("*", cors(corsConfig));
+app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept,authorization"
+  );
+  next();
+});
 
 //MongoDb Server Link
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.huhup.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
